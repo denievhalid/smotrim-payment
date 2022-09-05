a43be3615e7c74108aca548bf93a6f54b073750b
Halid <denievhalid88@mail.ru>
2022-09-05 21:02:10 +0300

    init

--- src/components/PaymentSuccess.vue
+++ src/components/PaymentSuccess.vue
@@ -1,4 +1,40 @@
 <template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="50px"
+    height="50px"
+    viewBox="0 0 50 50"
+    style="enable-background: new 0 0 50 50; margin-bottom: 20px"
+    xml:space="preserve"
+  >
+    <circle style="fill: #ff9600" cx="25" cy="25" r="25" />
+    <polyline
+      style="
+        fill: none;
+        stroke: #ffffff;
+        stroke-width: 2;
+        stroke-linecap: round;
+        stroke-linejoin: round;
+        stroke-miterlimit: 10;
+      "
+      points="  38,15 22,33 12,25 "
+    />
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+    <g></g>
+  </svg>
   <div>Оплата успешно проведена</div>
 </template>
 
